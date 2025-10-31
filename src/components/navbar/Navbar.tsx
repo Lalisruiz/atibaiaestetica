4b8cc1358033bbc008ba7fbe6943ada1f4f7b357
Matheus97px <matheus97p.q@gmail.com>
2025-10-31 17:30:47 -0300

    fix: colors navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,20 +8,20 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="container mx-auto px-4 py-4 bg-linear-to-b from-[#F4E4C1] to-[#D4AF37]">
+        <nav className=" mx-auto px-4 py-4 bg-linear-to-b from-[#F4E4C1] to-[#D4AF37] ">
             <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-2">
-                    <span className="text-2xl font-bold text-white hover:text-black transition hover:cursor-pointer">
-                      LR Estética
-                    </span>
+                  <a href="#home">
+                    <img className='w-60 ml-5' src="https://i.ibb.co/99601sS7/logolrestetica-Photoroom-1.png" alt="logo atibaia estetica" />
+                    </a>
                 </div>
                 
-                <ul className="hidden md:flex space-x-8 text-white font-bold">
-                    <li><a href="#home" className="hover:text-[#2C2416] transition">Home</a></li>
-                    <li><a href="#especialidades" className="hover:text-[#2C2416] transition">Especialidades</a></li>
-                    <li><a href="#sobre" className="hover:text-[#2C2416] transition">Sobre</a></li>
-                    <li><a href="#espaco" className="hover:text-[#2C2416] transition">Espaço</a></li>
-                    <li><a href="#transformacoes" className="hover:text-[#2C2416] transition">Transformações</a></li>
+                <ul className="hidden md:flex space-x-8 mt-4 text-[#2C2416] font-bold">
+                    <li><a href="#home" className="hover:text-[#FCFEFE] transition">Home</a></li>
+                    <li><a href="#especialidades" className="hover:text-[#FCFEFE] transition">Especialidades</a></li>
+                    <li><a href="#sobre" className="hover:text-[#FCFEFE] transition">Sobre</a></li>
+                    <li><a href="#espaco" className="hover:text-[#FCFEFE] transition">Espaço</a></li>
+                    <li><a href="#transformacoes" className="hover:text-[#FCFEFE] transition">Transformações</a></li>
                 </ul>
       
                 <button 
